9a18a2d3e9367272e99a13ee57d109c23167a6e0
Guilherme Lucio Silva <61800391+guilhermeluciosilva@users.noreply.github.com>
2021-07-14 11:33:45 -0300

    atualizando versao

---
+++ app/Libraries/Rota.php
@@ -0,0 +1,33 @@
+<?php
+
+class Rota {
+
+    private $controlador = 'Paginas';
+
+    public function __construct()
+    {
+        $url = $this->url() ?? [0];
+
+        if(file_exists('../app/Controllers/'.ucwords($url[0]).'.php')):
+            $this->controlador = ucwords($url[0]);
+            unset($url[0]);
+        endif;
+
+        require_once '../app/Controllers/'.$this->controlador.'.php';
+        $this->controlador = new $this->controlador;
+
+        var_dump($this);
+       
+    }
+
+    private function url()
+    {
+        $url =filter_input(INPUT_GET,'url', FILTER_SANITIZE_URL);
+        if(isset($url))
+        {
+            $url = trim(rtrim($url,'/'));
+            $url = explode('/', $url);
+            return $url;
+        } 
+    }
+}
---
+++ public/index.php
@@ -0,0 +1,19 @@
+<?php
+include './../app/Libraries/Rota.php';
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP7-MVC</title>
+</head>
+<body>
+    <?php    
+        $rotas = new Rota();
+        
+    ?>
+</body>
+</html>
