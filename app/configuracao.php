09293a8b266b0c0df936e91b9b73db2de2b3ee53
Guilherme Lucio Silva <61800391+guilhermeluciosilva@users.noreply.github.com>
2021-07-15 14:20:51 -0300

    atualizando versao

--- app/configuracao.php
+++ app/configuracao.php
@@ -1,3 +1,9 @@
 <?php
 
-echo dirname(__FILE__);
+define('APP', dirname(__FILE__));
+
+define('URL', 'http://localhost/PHP7-MVC');
+
+define('APP_NOME', 'PHP7 MVC');
+
+const APP_VERSAO = '1.0.0';
--- public/index.php
+++ public/index.php
@@ -10,12 +10,17 @@ include './../app/Libraries/Controller.php';
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP7-MVC</title>
+    <title><?= APP_NOME ?></title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"> 
+    <link href="<?=URL?>/public/css/estilos.css" rel="stylesheet"> 
 </head>
 <body>
     <?php    
-        $rotas = new Rota();
-        
+        $rotas = new Rota();    
     ?>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" ></script>
+    <script src="<?=URL?>/public/js/jquery.funcoes.js" ></script>
 </body>
 </html>
