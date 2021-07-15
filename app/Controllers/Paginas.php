721ef3aeb70b587ac2387d00f153559fe368f137
Guilherme Lucio Silva <61800391+guilhermeluciosilva@users.noreply.github.com>
2021-07-15 13:43:05 -0300

    atualizacao

--- app/Controllers/Paginas.php
+++ app/Controllers/Paginas.php
@@ -3,11 +3,19 @@
 class Paginas extends Controller {
 
     public function index(){
-        $this->view('paginas/home');
+        $dados = [
+            'tituloPagina' => 'Página Inicial',
+        ];
+
+        $this->view('paginas/home', $dados);
     }
 
 
     public function sobre(){
-     
+        $dados = [
+            'tituloPagina' => 'Página Sobre nós',
+        ];
+
+        $this->view('paginas/sobre', $dados);
     }
 }
--- public/index.php
+++ public/index.php
@@ -1,4 +1,5 @@
 <?php
+include './../app/configuracao.php';
 include './../app/Libraries/Rota.php';
 include './../app/Libraries/Controller.php';
 ?>
