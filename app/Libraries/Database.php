9f159b76e42e0fad8b2d452cb6cb6c9055db324d
Guilherme Lucio Silva <61800391+guilhermeluciosilva@users.noreply.github.com>
2021-07-16 15:36:37 -0300

    atualizando versao

--- app/Libraries/Database.php
+++ app/Libraries/Database.php
@@ -8,6 +8,8 @@ class Database{
     private $banco = 'postgres';
     private $porta = '5432';
     private $dbh;
+    private $stmt;
+
     
     public function __construct()
     {
@@ -27,6 +29,51 @@ class Database{
         
     }
 
+    public function query($sql){
+        $this->stmt = $this->dnh->prepare($sql);
+    }
+
+    public function bind($parametro, $valor, $tipo = null){
+        if(is_null(($tipo))){
+            switch (true){
+                case is_int($valor):
+                    $tipo = PDO::PARAM_INT;
+                break;
+                case is_bool($valor): 
+                    $tipo = PDO::PARAM_BOOL;
+                break;
+                case is_null($valor): 
+                    $tipo = PDO::PARAM_NULL;
+                break;
+                default: 
+                $tipo = PDO::PARAM_STR;
+            }
+        }
+
+        $this->stmt->bindValue($parametro, $valor, $tipo);
+    }
+
+    public function executa(){
+        return $this->stmt->execute();
+    }
+
+    public function resultado(){
+        $this->executa();
+        return $this->stmt->fetch(PDO::FETCH_OBJ);
+    }
+
+    public function resultados(){
+        $this->executa();
+        return $this->stmt->fetchAll(PDO::FETCH_OBJ);
+    }
+
+    public function totalResultados(){
+        return $this->stmt->rowCount();
+    }
+
+    public function ultimoIdInserido(){
+        return $this->stmt->lastInsertId();
+    }
+
+
 }
-
-
